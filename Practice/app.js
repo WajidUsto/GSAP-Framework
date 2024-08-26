59368555831840302126cf0b59a425362f1938d8
WajidUsto <wajidusto@gmail.com>
2024-08-27 03:23:05 +0500

    command added

--- Practice/app.js
+++ Practice/app.js
@@ -25,19 +25,28 @@
 //     stagger:0.3,
 // })
 var tl = gsap.timeline()
-tl.from("h2",{
-    x:20,
+tl.from("h3",{
+    y:-20,
     opacity:0,
     duration:1,
     delay:0.7,
 })
 
 tl.from("#h4 h4",{
-    // y:-30,
-    x:20,
+    y:-20,
     duration:1,
     opacity:0,
     stagger:0.3,
+})
+tl.from("h1",{
+    y:-20,
+    duration:0.5,
+    opacity:0,
+})
+tl.from("h2",{
+    y:-20,
+    duration:0.5,
+    opacity:0,
 })
 
 
