30766594b34851dfbce7289f7b731a6654287d2a
WajidUsto <wajidusto@gmail.com>
2024-08-27 02:17:53 +0500

    command added

--- Practice/app.js
+++ Practice/app.js
@@ -6,20 +6,21 @@
 //     backgroundColor :"white",
 //     borderRadius:"35%",
 // })
-// gsap.from("#box2",{
-//     x:1000,
-//     duration:5,
-//     delay:2,
-//     // rotate:90,
-//     backgroundColor:"white",
-//     borderRadius:"35%",
-// })
-
-
-gsap.from("h1",{
-    opacity:0,
+gsap.to("#box2",{
+    x:1000,
     duration:1,
     delay:1,
-    y:30,
-    stagger:1,
+    rotate:360,
+    yoyo: true,
+    repeat:-1,
+    backgroundColor:"black",
 })
+
+
+// gsap.from("h1",{
+//     opacity:0,
+//     duration:1,
+//     delay:0,
+//     y:30,
+//     stagger:0.3,
+// })
