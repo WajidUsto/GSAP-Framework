3ad97a678b28849d8e44a2f676fd697052e8453e
WajidUsto <wajidusto@gmail.com>
2024-09-01 01:55:10 +0500

    command added

--- Side Scrolling/app.js
+++ Side Scrolling/app.js
@@ -1,3 +1,12 @@
-gsap.from("#page2 h1",{
-    transform:()
+gsap.to("#page2 h1",{
+    transform:("translate(-100%)"),
+    scrollTrigger:{
+        trigger:"#page2",
+        scroller:"body",
+        // markers:true,
+        start:"top 0%",
+        end:"top -100%",
+        scrub:2,
+        pin:true,
+    }
 })
