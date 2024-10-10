2b6d48a6484b752ad724b3c4e83cc1f0961ff5de
Wajid Usto <wajidusto@gmail.com>
2024-10-10 14:09:18 +0500

    command added

---
+++ Custom Cursor/app.js
@@ -0,0 +1,13 @@
+let cursor = document.querySelector("#cursor")
+let main = document.querySelector("#main")
+
+
+main.addEventListener("mousemove",function (dets) {
+    gsap.to(cursor,{
+        x: dets.x,
+        y: dets.y,
+        ease:"back.out",
+        duration:1,
+    })
+})
+
