fb698b57bd6251356fe47220a44a87ad3e8467a6
Wajid Usto <wajidusto@gmail.com>
2024-10-11 20:22:19 +0500

    command added

---
+++ Text Animation/app.js
@@ -0,0 +1,17 @@
+
+
+
+gsap.from("#main .a",{
+    y : 40,
+    opacity : 0,
+    duration : 0.8,
+    delay : 0.5,
+    stagger : 0.2
+})
+gsap.from("#main .b",{
+    y : 40,
+    opacity : 0,
+    duration : 0.8,
+    delay : 0.5,
+    stagger : -0.2
+})
