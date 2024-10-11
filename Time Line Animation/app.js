b214bfde70b09cdd0bd446748a7ab3d03a5c308e
Wajid Usto <wajidusto@gmail.com>
2024-10-11 17:57:58 +0500

    command added

--- Time Line Animation/app.js
+++ Time Line Animation/app.js
@@ -1,5 +1,5 @@
 let menu = document.querySelector("#head-i")
-let close = document.querySelector("#sidebar i")
+let close = document.querySelector("#sidebar-i")
 let sidebar = document.querySelector("#sidebar")
 
 let tl = gsap.timeline()
@@ -19,19 +19,26 @@ let tl = gsap.timeline()
 
 tl.to(sidebar,{
     right : 0,
-    duration : 0.8,
+    duration : 0.6,
     
 })
 
 tl.from("#sidebar h3",{
     x:60,
-    duration : 0.8,
-    stagger : 0.2,
+    duration : 0.6,
+    stagger : 0.3,
     opacity : 0
 })
-
-
 tl.from(close,{
     opacity : 0,
     duration : 0.8,
 })
+
+tl.pause()
+
+menu.addEventListener("click",function () {
+        tl.play()
+    })
+close.addEventListener("click",function () {
+        tl.reverse()
+    })
